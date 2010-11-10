e98a9e90d372747ff644ba87631eede61b5cb3c7
Michael Spang <mspang@csclub.uwaterloo.ca>
2010-11-10 08:11:16 -0500

    Update iotest.c for TS-7800

--- iotest.c
+++ iotest.c
@@ -3,18 +3,17 @@
   */
 
 #include <bwio.h>
-#include <ts7200.h>
+#include <ts7800.h>
 
 int main(int argc, char *argv[]) {
-    char str[] = "Hello\n\r";
-    bwsetfifo(COM2, OFF);
-    bwputstr(COM2, str);
-    bwputw(COM2, 10, '*', str);
-    bwprintf(COM2, "Hello world.\n\r");
-    bwprintf(COM2, "%s world%u.\n\r", "Well, hello", 23);
-    bwprintf(COM2, "%d worlds for %u person.\n\r", -23, 1);
-    bwprintf(COM2, "%x worlds for %d people.\n\r", -23, 723);
-    str[0] = bwgetc(COM2);
-    bwprintf(COM2, "%s", str);
+    char *str = "Hello\r\n";
+    bwputstr(COM1, str);
+    bwputw(COM1, 10, '*', str);
+    bwprintf(COM1, "Hello world.\r\n");
+    bwprintf(COM1, "%s world%u.\r\n", "Well, hello", 23);
+    bwprintf(COM1, "%d worlds for %u person.\r\n", -23, 1);
+    bwprintf(COM1, "%x worlds for %d people.\r\n", -23, 723);
+    str[0] = bwgetc(COM1);
+    bwprintf(COM1, "You typed: %c\r\n", str[0]);
     return 0;
 }
