98eca3f66cab1b8d736112f4f135441cfff8ed39
Brennan Taylor <taylor.brennan@gmail.com>
2011-05-25 13:00:48 -0400

    Making output less verbose and adding some color.

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all:
-	cd lib && $(MAKE)
-	cd drivers && $(MAKE)
-	cd kern && $(MAKE) upload
+	@cd lib && echo "Building lib" && $(MAKE) TOP=lib/ --no-print-directory
+	@cd drivers && echo "Building drivers" && $(MAKE) TOP=drivers/ --no-print-directory
+	@cd kern && echo "Building kern" && $(MAKE) TOP=kern/ --no-print-directory
 
 clean:
-	cd lib && $(MAKE) clean
-	cd drivers && $(MAKE) clean
-	cd kern && $(MAKE) clean
-	-rm -rf bin
+	@cd lib && $(MAKE) clean --no-print-directory --no-builtin-rules
+	@cd drivers && $(MAKE) clean --no-print-directory --no-builtin-rules
+	@cd kern && $(MAKE) clean --no-print-directory --no-builtin-rules
+	-@rm -rf bin
--- defs.mk
+++ defs.mk
@@ -4,8 +4,10 @@ AS      = $(XPREFIX)as
 LD      = $(XPREFIX)ld
 AR      = $(XPREFIX)ar
 
+ECHO = echo
+
 # Standard options
-CFLAGS  += -Wall -I$(S)/include -std=gnu99 -O2 -s
+CFLAGS  += -pipe -Wall -I$(S)/include -std=gnu99 -O2 -s
 
 # ARMv7 instruction set, Cortex-A8 tuning
 CFLAGS  += -march=armv7-a -mtune=cortex-a8
@@ -43,39 +45,46 @@ ifeq ($(DEBUG),1)
   CFLAGS += -g -DDEBUG -Wno-unused-function
 endif
 
+RED = 2>&1 1>/dev/null | sed -e "s;^;\x1b[31m$(TOP);" -e "s;$$;\x1b[m;"
 
 # New pattern rules
 $(TARGET) : $(OBJS) $(LIBS)
 	-@mkdir -p $(S)/bin
 ifeq ($(suffix $(TARGET)),.a)
-	$(AR) crs $@ $(OBJS)
+	@$(ECHO) "  [AR] $(TOP)$(TARGET)"
+	@$(AR) crs $@ $(OBJS) $(RED)
 else
-	$(XCC) $(CFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(LIBS) $(LDLIBS)
+	@$(ECHO) "  [CC] $(TOP)$(TARGET)"
+	@$(XCC) $(CFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(LIBS) $(LDLIBS) $(RED)
 endif
 
 obj/%.s: %.c
 	-@mkdir -p obj
-	$(XCC) -S $(CFLAGS) -o $@ $(filter %.c,$^)
+	@$(ECHO) "  [CC] $(TOP)$@"
+	@$(XCC) -S $(CFLAGS) -o $@ $(filter %.c,$^) $(RED)
 
 obj/%.o: obj/%.s
 	-@mkdir -p obj
-	$(XCC) -c $(CFLAGS) -o $@ $<
+	@$(ECHO) "  [AS] $(TOP)$@"
+	@$(XCC) -c $(CFLAGS) -o $@ $< $(RED)
 
 obj/%.o: %.S
 	-@mkdir -p obj
-	$(XCC) -c $(CFLAGS) -o $@ $(filter %.S,$^)
+	@$(ECHO) "  [AS] $(TOP)$@"
+	@$(XCC) -c $(CFLAGS) -o $@ $(filter %.S,$^) $(RED)
 
+# Dep generation should be silent
 obj/%.dep: %.c
 	-@mkdir -p obj
-	$(SHELL) -ec '$(XCC) -S -MT $(<:%.c=%.s) -M $(CFLAGS) $< | sed "s/$*.s/obj\\/$*.s obj\\/$*.dep/g" > $@'
+	@$(SHELL) -ec '$(XCC) -S -MT $(<:%.c=%.s) -M $(CFLAGS) $< | sed "s/$*.s/obj\\/$*.s obj\\/$*.dep/g" > $@'
 
 obj/%.dep: %.S
 	-@mkdir -p obj
-	$(SHELL) -ec '$(XCC) -S -M $(CFLAGS) $< | sed "s/$*.S/& obj\\/$*.dep/g" > $@'
+	@$(SHELL) -ec '$(XCC) -S -M $(CFLAGS) $< | sed "s/$*.S/& obj\\/$*.dep/g" > $@'
 
 clean:
-	-rm -rf obj
-	-rm $(TARGET)
+	-@rm -rf obj
+	-@rm -f $(TARGET)
 
 # Cancel .c shortcut rule
 %.o: %.c
