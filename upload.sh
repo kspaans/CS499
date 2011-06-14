4c8bf7bff6fe29a28a59bd9aabbbb779c8e270ba
Robert Xiao <nneonneo@gmail.com>
2011-06-13 23:38:18 -0400

    fixing upload script to use bin/

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CMD=`which redo` || CMD="./utils/do"
-${CMD} all && scp kern/kern.elf gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
+${CMD} all && scp bin/kern.elf gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
