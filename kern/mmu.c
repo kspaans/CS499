57b2073de311ced198411ae76c44fe7a9ffbf702
Kyle Spaans <kspaans@uwaterloo.ca>
2011-06-17 00:44:48 -0400

    More debugging of the MMU code, still not working

--- kern/mmu.c
+++ kern/mmu.c
@@ -10,7 +10,14 @@
 #include <kern/printk.h>
 
 
-// Page table entries -- "First Level Descriptors"
+#define FLD_CACHABLE
+#define FLD_BUFFERABLE
+#define FLD_SHAREABLE
+#define FLD_EXECNEVER
+#define FLD_GLOBAL
+#define FLD_NEVERSHARE
+
+// Page table entries -- "First Level Descriptors"  FLAGIFY ME
 // Device memory
 #define FLD48 0x480c0c06
 #define FLD49 0x490c0c06
@@ -19,9 +26,9 @@
 // Normal memory
 #define FLD40 0x400c0c0a
 #define FLD80 0x800c0c0a
-#define FLD8F 0x8f0c0c0a
+#define FLD90 0x900c0c0a
 
-uint32_t pagetable[0x4000] __attribute__((aligned(0x4000)));
+uint32_t pagetable[0x1000] __attribute__((aligned(0x4000)));
 
 // 16 * 1024 needed for the page table, 14 bits of stuff see docs/mmu.txt for
 // which addresses need to be in the table
@@ -34,46 +41,34 @@ void prep_pagetable()
 {
 	uint32_t i;
 
-	printk("PAGETABLE AT %p\r\n", pagetable);
-	memset(pagetable, 0, 16 * 1024); // start will invalid FLDs -- all 0s
+	printk("PAGETABLE AT %p -- size %d bytes\n", pagetable, sizeof(pagetable));
+	// could have fucked up the "N" part for the TTBR#
+	memset(pagetable, 0, sizeof(pagetable)); // start will invalid FLDs -- all 0s
 
 	// The top 14 bits of the VA make up the table index, used here to add
 	// the entries and their replicas.
-	pagetable[0x1000] = FLD40;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x1000 + i] = FLD40;
+	for (i = 0; i < 16; i += 1) {
+		pagetable[0x0ac0 / 4 + i] = FLD2B;
+		pagetable[0x0b00 / 4 + i] = FLD2C;
+		pagetable[0x1000 / 4 + i] = FLD40;
+		pagetable[0x1200 / 4 + i] = FLD48;
+		pagetable[0x1240 / 4 + i] = FLD49;
+		pagetable[0x2000 / 4 + i] = FLD80;
+		pagetable[0x2400 / 4 + i] = FLD90;
 	}
-	pagetable[0x2000] = FLD80;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x2000 + i] = FLD80;
-	}
-	pagetable[0x23c0] = FLD8F;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x23c0 + i] = FLD8F;
-	}
-	pagetable[0x1200] = FLD48;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x1200 + i] = FLD48;
-	}
-	pagetable[0x1240] = FLD49;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x1240 + i] = FLD49;
-	}
-	pagetable[0x0ac0] = FLD2B;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x0ac0 + i] = FLD2B;
-	}
-	pagetable[0x0b00] = FLD2C;
-	for (i = 1; i < 16; i += 1) {
-		pagetable[0x0b00 + i] = FLD2C;
-	}
-	memset(pagetable, 0, 16 * 1024); // start will invalid FLDs -- all 0s
+
+	//memset(pagetable, 0, sizeof(pagetable)); // start will invalid FLDs -- all 0s
 
 	set_ttbr(pagetable);
+	printk("TTBR setup\r");
 
 uint32_t ttb;
         asm ("mrc p15, 0, %0, c2, c0, 0" : "=r" (ttb));
 printk("ttb = %08x\n", ttb);
+	asm ("mrc p15, 0, %0, c1, c0, 0" : "=r" (ttb));
+printk("MMU = %08x\n", ttb);
+	asm ("MRC p15, 0, %0, c3, c0, 0" : "=r" (ttb));
+printk("DACR = %08x\n", ttb);
 
-
+	printk("Pagetables setup.\n");
 }
