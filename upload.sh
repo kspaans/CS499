ad1be41f112590da76ce99d008d7e714073e4401
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-18 01:50:58 -0400

    Build faster

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CMD=`which redo` || CMD="./utils/do"
-${CMD} all && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
+${CMD} all -j10 && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
