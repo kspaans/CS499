dc0663b1451f56b07b5eb00a21371c4d9f9c2740
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-22 16:54:01 -0400

    Build faster on corn-syrup

--- upload.sh
+++ upload.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 CMD=`which redo` || CMD="./utils/do"
-${CMD} all && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
+
+jobs=1
+
+case $(hostname -f) in
+	corn-syrup.csclub.uwaterloo.ca)
+		jobs=10
+		;;
+esac
+
+${CMD} -j${jobs} all && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
