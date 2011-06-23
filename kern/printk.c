4fd68be0ae54c6667415c3b539786616e723b346
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-23 01:08:55 -0400

    Panic on exception

--- kern/printk.c
+++ kern/printk.c
@@ -37,10 +37,28 @@ void panic(const char *fmt, ...) {
 
 	printk("\n");
 
+	sysrq();
+
 	/* hacks */
-	for (volatile int i = 0; i < 1000000; ++i);
+	for (volatile int i = 0; i < 10000000; ++i);
 
 	prm_reset();
+}
+
+void panic_pabt(void) {
+	panic("Prefetch Abort");
+}
+
+void panic_dabt(void) {
+	panic("Data Abort");
+}
+
+void panic_undef(void) {
+	panic("Undefined Instruction");
+}
+
+void panic_unused(void) {
+	panic("Unused exception vector");
 }
 
 int vprintk(const char *fmt, va_list va) {
