9133dd440a7ec1c5b77f424f47bf028542ab99e8
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-26 23:53:22 -0400

    Dense syscall numbers
    Saves 1ns.

--- include/kern/syscallno.h
+++ include/kern/syscallno.h
@@ -1,18 +1,18 @@
 #ifndef KERN_SYSCALLNO_H
 #define KERN_SYSCALLNO_H
 
-#define SYS_SPAWN        1
-#define SYS_GETTID         2
-#define SYS_GETPTID        3
-#define SYS_YIELD          4
-#define SYS_EXIT          5
-#define SYS_SEND          6
-#define SYS_RECV          7
-#define SYS_WAITEVENT    11
-#define SYS_TASKSTAT      12
-#define SYS_SUSPEND       13
-#define SYS_CHANNEL   14
-#define SYS_CLOSE  15
-#define SYS_DUP    16
+#define SYS_SPAWN      1
+#define SYS_GETTID     2
+#define SYS_GETPTID    3
+#define SYS_YIELD      4
+#define SYS_EXIT       5
+#define SYS_SEND       6
+#define SYS_RECV       7
+#define SYS_WAITEVENT  8
+#define SYS_TASKSTAT   9
+#define SYS_SUSPEND    10
+#define SYS_CHANNEL    11
+#define SYS_CLOSE      12
+#define SYS_DUP        13
 
 #endif /* KERN_SYSCALLNO_H */
