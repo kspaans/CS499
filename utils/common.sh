035f6c60ed6cdd2c2dc946f767c20ce676b29d6d
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-27 03:40:40 -0400

    Some common.sh cleanup

--- utils/common.sh
+++ utils/common.sh
@@ -26,16 +26,13 @@ else
 fi
 
 # Standard options
-CFLAGS="-pipe -Wall -Wextra -I include -std=gnu99 -O3"
+CFLAGS="-pipe -Wall -Wextra -Werror -I include -std=gnu99 -O3"
 
-# Debug options
-CFLAGS+=" -Werror"
-
-# ARMv7 instruction set, Cortex-A8 tuning
-CFLAGS+=" -march=armv7-a -mtune=cortex-a8"
+# ARMv7 instruction set, Cortex-A8 tuning, ARM instrutions
+CFLAGS+=" -march=armv7-a -mtune=cortex-a8 -marm"
 
 # Keep frame pointers
-CFLAGS+=" -fno-omit-frame-pointer -marm"
+CFLAGS+=" -fno-omit-frame-pointer"
 
 # Make assembly output more readable
 CFLAGS+=" -fverbose-asm"
@@ -43,10 +40,18 @@ CFLAGS+=" -fverbose-asm"
 # No C library
 CFLAGS+=" -ffreestanding"
 
+# Super ricer-mode
+#CFLAGS+=" -flto"
+
 if test -n "$DEBUG"; then
-	CFLAGS+=' -g -DDEBUG -Wno-unused-function -Wno-unused-parameter'
+	CFLAGS+=' -g -DDEBUG -Wno-unused'
 else
-	CFLAGS+=' -Wno-unused-parameter -Wmissing-prototypes -Wmissing-declarations -Wold-style-definition -Wstrict-prototypes -Winit-self -Wstrict-overflow -Wfloat-equal -Wshadow -Wpointer-arith -Wcast-align -Wwrite-strings -Wmissing-format-attribute -Wredundant-decls -Wnested-externs -Wvolatile-register-var -Wdisabled-optimization'
+	CFLAGS+=' -Wno-unused-parameter -Wmissing-prototypes -Wmissing-declarations'
+	CFLAGS+=' -Wold-style-definition -Wstrict-prototypes -Winit-self'
+	CFLGAS+=' -Wstrict-overflow -Wfloat-equal -Wshadow -Wpointer-arith'
+	CFLAGS+=' -Wcast-align -Wwrite-strings -Wmissing-format-attribute'
+	CFLAGS+=' -Wredundant-decls -Wnested-externs -Wvolatile-register-var'
+	CFLAGS+=' -Wdisabled-optimization'
 fi
 
 CFLAGS+=" -DBUILDUSER=$USER"
