d3e41d93359d967292b2efc55baa4b3c55e424df
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-29 13:05:41 -0400

    Fix upload.sh with do

--- upload.sh
+++ upload.sh
@@ -5,8 +5,8 @@ jobs=1
 
 case $(hostname) in
 	corn-syrup)
-		jobs=10
+		jobs=-j10
 		;;
 esac
 
-${CMD} -j${jobs} all && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
+${CMD} ${jobs} all && scp bin/kern gumstix.cs.uwaterloo.ca:/srv/tftp/ARM/${USER}/kern
