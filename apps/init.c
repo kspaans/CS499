e1a449ae94f86bb4eaa5e8dee3221e33ec758ca3
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-07-01 00:34:28 -0400

    We suck at POSIX

--- apps/init.c
+++ apps/init.c
@@ -20,10 +20,12 @@ void init_task(void) {
 
 	// any servers above this point will not be be able to use the netconsole
 	if (!this_host->has_uart) {
-		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
-		open(ROOT_DIRFD, "/dev/netconin");
-		open(ROOT_DIRFD, "/dev/netconout");
+		int netconin = open(ROOT_DIRFD, "/dev/netconin");
+		int netconout = open(ROOT_DIRFD, "/dev/netconout");
+		dup(netconin, STDIN_FILENO, 0);
+		dup(netconout, STDOUT_FILENO, 0);
+		close(netconin);
+		close(netconout);
 	}
 
 	genesis_init();
