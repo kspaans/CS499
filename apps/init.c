2044fb652105c9566745b0ca12473805dc33a357
Michael Spang <mspang@csclub.uwaterloo.ca>
2011-06-30 23:50:26 -0400

    Move tests to test.c

--- apps/init.c
+++ apps/init.c
@@ -14,39 +14,6 @@
 #include <apps.h>
 #include <drivers/pmu.h>
 
-#include <eth.h>
-#include <servers/net.h>
-__attribute__((unused)) static void udp_tx_loop(void) {
-	printf("Type characters to send to the remote host; Ctrl+D to quit\n");
-
-	int netconout = open(ROOT_DIRFD, "/dev/netconout");
-
-	fprintf(netconout, "Hello from %s\n", this_host->hostname);
-
-	for(;;) {
-		char c = getchar();
-		if(c == 4)
-			return;
-		if(c == '\r')
-			c = '\n';
-		fputc(c, STDOUT_FILENO);
-		fputc(c, netconout);
-	}
-}
-
-#include <eth.h>
-#include <servers/net.h>
-__attribute__((unused)) static void udp_rx_loop(void) {
-	int netconin = open(ROOT_DIRFD, "/dev/netconin");
-	int netconout = open(ROOT_DIRFD, "/dev/netconout");
-
-	for(;;) {
-		char c = fgetc(netconin);
-		fputc(c, STDOUT_FILENO);
-		fputc(c, netconout);
-	}
-}
-
 #include <lib.h>
 #include <string.h>
 #include <timer.h>
@@ -132,71 +99,6 @@ __attribute__((unused)) static void srrbench_task(void) {
 #undef BENCHSRR
 
 	printf("srrbench_task[%d]: benchmark finished.\n", tid);
-}
-
-static void null_task(void) {
-}
-static void task_reclamation_2(void) {
-	for(int i=0; i<1000; ++i) {
-		printk("%05x ", spawn(5, null_task, 0));
-		printk("%05x\n", spawn(5, null_task, 0));
-	}
-}
-__attribute__((unused)) static void task_reclamation_test(void) {
-	for(int i=0; i<1000; ++i) {
-		printk("%05x ", spawn(1, null_task, 0));
-		printk("%05x ", spawn(1, null_task, 0));
-		printk("%05x\n", spawn(3, null_task, 0));
-	}
-	spawn(4, task_reclamation_2, 0);
-}
-
-static uint32_t dumbhash(int x) { return 0; }
-static void hashtable_print(hashtable *ht) {
-	printf("{");
-	for(uint32_t i=0; i<ht->max; i++) {
-		if(!ht->arr[i].valid)
-			printf("*");
-		else if(ht->arr[i].deleted)
-			printf("x");
-		else
-			printf("%d:%p", ht->arr[i].key, ht->arr[i].value);
-		if(i != ht->max-1)
-			printf(",");
-	}
-	printf("}\n");
-}
-__attribute__((unused)) static void hashtable_test(void) {
-	hashtable ht;
-	struct ht_item ht_arr[3];
-	hashtable_init(&ht, ht_arr, 3, dumbhash, NULL);
-#define GET(i) { int k = (i); printf("get %d -> %d: ", k, hashtable_get(&ht, k, NULL)); hashtable_print(&ht); }
-#define PUT(i,j) { int k = (i); void *l = (void *)(j); printf("put %d -> %d: ", k, hashtable_put(&ht, k, l)); hashtable_print(&ht); }
-#define DEL(i) { int k = (i); printf("del %d -> %d: ", k, hashtable_del(&ht, k)); hashtable_print(&ht); }
-	GET(0); PUT(0,0); GET(0); DEL(0);
-	PUT(3,1); PUT(5,2); PUT(0,3); PUT(1,4); PUT(3,0); PUT(5,5); DEL(0); DEL(3); DEL(1); DEL(5);
-#undef GET
-#undef PUT
-#undef DEL
-}
-
-__attribute__((unused)) static void fstest_task(void) {
-	int chan;
-	char path[PATH_MAX+1];
-	printf("path_max test\n");
-	memset(path, 'a', PATH_MAX);
-	path[PATH_MAX] = 0;
-	printf(" mkchan: %d\n", mkchan(ROOT_DIRFD, path));
-	printf(" open: %d\n", chan=open(ROOT_DIRFD, path));
-	printf(" rmchan: %d\n", rmchan(ROOT_DIRFD, path));
-	printf(" close: %d\n", close(chan));
-	for(int i=0; i<100; ++i) {
-		sprintf(path, "/tmp/test%d", i);
-		ASSERTNOERR(mkchan(ROOT_DIRFD, path));
-		ASSERTNOERR(chan=open(ROOT_DIRFD, path));
-		ASSERTNOERR(rmchan(ROOT_DIRFD, path));
-		ASSERTNOERR(close(chan));
-	}
 }
 
 /* The first user program */
--- bin/apps.a.do
+++ bin/apps.a.do
@@ -2,6 +2,7 @@
 
 DEPS=(apps/gameoflife.c
       apps/shell.c
-      apps/init.c)
+      apps/init.c
+      apps/test.c)
 
 ../utils/archive $3 ${DEPS[@]/#/../}
